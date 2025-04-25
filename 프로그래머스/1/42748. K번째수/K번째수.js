3ff1e3ba08cf12b7c3a4b0b33f46e52f75f35f4b
Seeun Jung <141086226+SeeunJung@users.noreply.github.com>
2025-04-25 14:28:53 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    let answer = [];
+    for(let l=0; l<commands.length; l++){
+        let [i, j, k] = commands[l];
+        let newArr = array.slice(i-1, j).sort((a, b) => a-b);
+        answer.push(newArr[k-1]);
+    }
+    return answer;
+}
