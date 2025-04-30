a839d7c1cf26a394ee4786eb493d6051bc58a1cf
Seeun Jung <141086226+SeeunJung@users.noreply.github.com>
2025-04-30 15:50:18 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,14 @@
+function solution(cards1, cards2, goal) {
+
+    for(const g of goal) {
+        if(cards1[0] == g) {
+            cards1.shift();
+        } else if(cards2[0] == g) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
+    }
+
+    return "Yes";
+}
