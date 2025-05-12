97fa78bc1dd01e7f84b247d0c37411abbade15b7
Seeun Jung <141086226+SeeunJung@users.noreply.github.com>
2025-05-12 14:47:34 +0900

    [level 2] Title: 올바른 괄호, Time: 7.04 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,17 @@
+function solution(s){
+    let stack = [];
+    
+    for(const c of s){
+        if(c === "(")
+            stack.push(c);
+        else if(c === ")"){
+            if(stack.length === 0){
+                return false;   
+            }
+            else{
+                stack.pop();
+            }
+        }
+    }
+    return stack.length === 0;
+}
